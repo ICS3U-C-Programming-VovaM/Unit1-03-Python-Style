acbfebd5e363c031fa85bf852df25756f9d7f257
Vova Muzychko <volodymyr.muzychko@stu.ocsb.ca>
2026-09-15 13:33:32 +0000

    First commit of hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Created By: VovaM
+# Date: Sep 15th, 2026
+# This program displays a greeting and contains proper formatting
+def main():
+    print ("Hello,Ms Raffin")
+    print("Programming is cool")
+
+
+if __name__ == "__main__":
+
+    main()
